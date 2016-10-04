e728db39e47db876f308558958c85d098aa0bce8
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-10-04 09:21:43 -0400

    puppet-lint++

--- nubis/puppet/prometheus.pp
+++ nubis/puppet/prometheus.pp
@@ -1,52 +1,52 @@
-$prometheus_version = "1.0.1"
-$alertmanager_version = "0.4.0"
+$prometheus_version = '1.0.1'
+$alertmanager_version = '0.4.0'
 
 $prometheus_url = "https://github.com/prometheus/prometheus/releases/download/v${prometheus_version}/prometheus-${prometheus_version}.linux-amd64.tar.gz"
 $alertmanager_url = "https://github.com/prometheus/alertmanager/releases/download/v${alertmanager_version}/alertmanager-${alertmanager_version}.linux-amd64.tar.gz"
 
-file { "/opt/prometheus":
+file { '/opt/prometheus':
   ensure => 'directory',
   owner  => 0,
   group  => 0,
-  mode   => 755,
+  mode   => '0755',
 }
 
-file { "/etc/prometheus":
+file { '/etc/prometheus':
   ensure => 'directory',
   owner  => 0,
   group  => 0,
-  mode   => 755,
+  mode   => '0755',
 }->
-file { "/etc/prometheus/rules.d":
+file { '/etc/prometheus/rules.d':
   ensure => 'directory',
   owner  => 0,
   group  => 0,
-  mode   => 755,
+  mode   => '0755',
 }
 
-file { "/etc/prometheus/rules.d/nubis.prom":
+file { '/etc/prometheus/rules.d/nubis.prom':
     ensure  => file,
     owner   => root,
     group   => root,
     mode    => '0755',
-    source  => 'puppet:///nubis/files/rules/nubis.prom',
+    source  => 'puppet:///modules/nubis/files/rules/nubis.prom',
     require => File['/etc/prometheus/rules.d'],
 }
 
-file { "/var/lib/prometheus":
+file { '/var/lib/prometheus':
   ensure => 'directory',
   owner  => 0,
   group  => 0,
-  mode   => 755,
+  mode   => '0755',
 }
 
 # bootup prometheus actions
 file { '/etc/nubis.d/prometheus':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0755',
-    source  => 'puppet:///nubis/files/prometheus-restart',
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/nubis/files/prometheus-restart',
 }
 
 file { '/etc/prometheus/config.yml':
@@ -54,7 +54,7 @@ file { '/etc/prometheus/config.yml':
     owner   => root,
     group   => root,
     mode    => '0644',
-    source  => 'puppet:///nubis/files/prometheus.yml',
+    source  => 'puppet:///modules/nubis/files/prometheus.yml',
     require => File['/etc/prometheus'],
 }
 
@@ -63,7 +63,7 @@ file { '/etc/prometheus/alertmanager.yml':
     owner   => root,
     group   => root,
     mode    => '0644',
-    source  => 'puppet:///nubis/files/alertmanager.yml',
+    source  => 'puppet:///modules/nubis/files/alertmanager.yml',
     require => File['/etc/prometheus'],
 }
 
@@ -72,32 +72,32 @@ file { '/etc/init/prometheus.conf':
     owner   => root,
     group   => root,
     mode    => '0644',
-    source  => 'puppet:///nubis/files/prometheus.upstart',
+    source  => 'puppet:///modules/nubis/files/prometheus.upstart',
     require => File['/etc/prometheus'],
 }
 
 file { '/etc/consul/svc-prometheus.json':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    source  => 'puppet:///nubis/files/svc-prometheus.json',
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/nubis/files/svc-prometheus.json',
 }
 
 file { '/etc/consul/svc-grafana.json':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    source  => 'puppet:///nubis/files/svc-grafana.json',
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/nubis/files/svc-grafana.json',
 }
 
 file { '/etc/consul/svc-alertmanager.json':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    source  => 'puppet:///nubis/files/svc-alertmanager.json',
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/nubis/files/svc-alertmanager.json',
 }
 
 notice ("Grabbing prometheus ${prometheus_version}")
@@ -106,9 +106,9 @@ staging::file { "prometheus.${prometheus_version}.tar.gz":
 }->
 staging::extract { "prometheus.${prometheus_version}.tar.gz":
   strip   => 1,
-  target  => "/opt/prometheus",
-  creates => "/opt/prometheus/prometheus",
-  require => File["/opt/prometheus"],
+  target  => '/opt/prometheus',
+  creates => '/opt/prometheus/prometheus',
+  require => File['/opt/prometheus'],
 }
 
 notice ("Grabbing alertmanager ${alertmanager_version}")
@@ -117,54 +117,54 @@ staging::file { "alertmanager.${alertmanager_version}.tar.gz":
 }->
 staging::extract { "alertmanager.${alertmanager_version}.tar.gz":
   strip   => 1,
-  target  => "/opt/prometheus",
-  creates => "/opt/prometheus/alertmanager",
-  require => File["/opt/prometheus"],
+  target  => '/opt/prometheus',
+  creates => '/opt/prometheus/alertmanager',
+  require => File['/opt/prometheus'],
 }
 
 # XXX: This is just too ugly
-exec { "apt-get-update-grafana":
-  command => "/usr/bin/apt-get update",  
+exec { 'apt-get-update-grafana':
+  command => '/usr/bin/apt-get update',
 }->
 class { 'grafana':
-  install_method  => 'repo',
-  cfg => {
+  install_method => 'repo',
+  cfg            => {
     app_mode => 'production',
     users    => {
       allow_sign_up => false,
     },
   },
 }->
-exec {"wait-for grafana startup":
-  command => "/bin/sleep 15",
+exec {'wait-for grafana startup':
+  command => '/bin/sleep 15',
 }->
 grafana_datasource { 'prometheus':
-  grafana_url       => 'http://localhost:3000',
-  grafana_user      => 'admin',
-  grafana_password  => 'admin',
-  type              => 'prometheus',
-  url               => 'http://localhost:80',
-  access_mode       => 'proxy',
-  is_default        => true,
+  grafana_url      => 'http://localhost:3000',
+  grafana_user     => 'admin',
+  grafana_password => 'admin',
+  type             => 'prometheus',
+  url              => 'http://localhost:80',
+  access_mode      => 'proxy',
+  is_default       => true,
 }
 
 include 'upstart'
 
 upstart::job { 'alertmanager':
-    description    => 'Prometheus Alert Manager',
+    description   => 'Prometheus Alert Manager',
 #    service_ensure => 'stopped',
     # Never give up
-    respawn        => true,
-    respawn_limit  => 'unlimited',
-    start_on       => '(local-filesystems and net-device-up IFACE!=lo)',
-    env            => {
-      "SLEEP_TIME" => 1,
-      "GOMAXPROCS" => 2,
+    respawn       => true,
+    respawn_limit => 'unlimited',
+    start_on      => '(local-filesystems and net-device-up IFACE!=lo)',
+    env           => {
+      'SLEEP_TIME' => 1,
+      'GOMAXPROCS' => 2,
     },
-    user           => 'root',
-    group          => 'root',
-    exec           => '/opt/prometheus/alertmanager -config.file /etc/prometheus/alertmanager.yml',
-    post_stop      => '
+    user          => 'root',
+    group         => 'root',
+    exec          => '/opt/prometheus/alertmanager -config.file /etc/prometheus/alertmanager.yml',
+    post_stop     => '
 goal=$(initctl status $UPSTART_JOB | awk \'{print $2}\' | cut -d \'/\' -f 1)
 if [ $goal != "stop" ]; then
     echo "Backoff for $SLEEP_TIME seconds"
