a95d677495a100f4247d4a09e2139d7e3a65ff8d
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-11-30 14:09:23 -0500

    Convert to confd-driven configuration for services

---
+++ nubis/puppet/grafana.pp
@@ -0,0 +1,78 @@
+package {'crudini':
+  ensure => present,
+}
+
+# XXX: This is just too ugly
+#exec { 'apt-get-update-grafana':
+#  command => '/usr/bin/apt-get update',
+#}->
+class { 'grafana':
+  install_method => 'repo',
+  version        => '3.1.1-1470047149',
+  cfg            => {
+    app_mode          => 'production',
+    'server' => {
+      protocol => 'http',
+      root_url => '/grafana',
+    },
+    'auth.anonymous'  => {
+      enabled => true,
+    },
+    # Needs to be disabled for traefik, enabled for grafana_datasource, hurgh
+    'auth.basic'      => {
+      enabled => true,
+    },
+    users             => {
+      allow_sign_up => false,
+    },
+    'dashboards.json' => {
+      enabled => true,
+    },
+  },
+}->
+exec {'wait-for grafana startup':
+  command => '/bin/sleep 15',
+}->
+grafana_datasource { 'prometheus':
+  grafana_url      => 'http://localhost:3000',
+  grafana_user     => 'admin',
+  grafana_password => 'admin',
+  type             => 'prometheus',
+  url              => 'http://localhost:81',
+  access_mode      => 'proxy',
+  is_default       => true,
+}->
+#grafana_datasource { 'cloudwatch': is not supported ;-(
+exec {'create cloudwatch datasource':
+  command => '/usr/bin/curl -u admin:admin -H \'Content-Type: application/json\' http://localhost:3000/api/datasources -X POST --data-binary  \'{"name": "cloudwatch", "type":"cloudwatch", "access": "proxy" }\''
+}->
+exec { 'disable basic auth':
+  command => '/usr/bin/crudini --set /etc/grafana/grafana.ini auth.basic enabled false',
+  require => [
+    Package['crudini'],
+  ]
+}->
+exec {'enable proxy support':
+  command => '/bin/echo ". /etc/profile.d/proxy.sh" >> /etc/default/grafana-server'
+}
+
+file { '/var/lib/grafana/dashboards':
+  ensure  => directory,
+  owner   => grafana,
+  group   => grafana,
+  mode    => '0640',
+  recurse => true,
+  purge   => true,
+  source  => 'puppet:///nubis/files/grafana/dashboards',
+  require => [
+    Class['grafana'],
+  ]
+}
+
+file { '/etc/consul/svc-grafana.json':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///nubis/files/svc-grafana.json',
+}
---
+++ nubis/puppet/traefik.pp
@@ -0,0 +1,83 @@
+$traefik_version = '1.1.1'
+$traefik_url = "https://github.com/containous/traefik/releases/download/v${traefik_version}/traefik_linux-amd64"
+
+notice ("Grabbing traefik ${traefik_version}")
+
+staging::file { '/usr/local/bin/traefik':
+  source => $traefik_url,
+  target => '/usr/local/bin/traefik',
+}->
+exec { "chmod /usr/local/bin/traefik":
+  command => "chmod 755 /usr/local/bin/traefik",
+  path => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
+}
+
+file { '/etc/traefik':
+  ensure => directory,
+  owner  => root,
+  group  => root,
+  mode   => '0640',
+}
+
+package {'apache2-utils':
+  ensure => '2.4.7-1ubuntu4.13'
+}
+
+upstart::job { 'traefik':
+    description    => 'Traefik Load Balancer',
+    service_ensure => 'stopped',
+    require        => [
+      Staging::File['/usr/local/bin/traefik'],
+    ],
+    # Never give up
+    respawn        => true,
+    respawn_limit  => 'unlimited',
+    start_on       => '(local-filesystems and net-device-up IFACE!=lo)',
+    env            => {
+      'SLEEP_TIME' => 1,
+      'GOMAXPROCS' => 2,
+    },
+    user           => 'root',
+    group          => 'root',
+    script         => '
+  if [ -r /etc/profile.d/proxy.sh ]; then
+    echo "Loading Proxy settings"
+    . /etc/profile.d/proxy.sh
+  fi
+
+  exec /usr/local/bin/traefik --web.readonly=true --loglevel=INFO
+',
+    post_stop      => '
+goal=$(initctl status $UPSTART_JOB | awk \'{print $2}\' | cut -d \'/\' -f 1)
+if [ $goal != "stop" ]; then
+    echo "Backoff for $SLEEP_TIME seconds"
+    sleep $SLEEP_TIME
+    NEW_SLEEP_TIME=`expr 2 \* $SLEEP_TIME`
+    if [ $NEW_SLEEP_TIME -ge 60 ]; then
+        NEW_SLEEP_TIME=60
+    fi
+    initctl set-env SLEEP_TIME=$NEW_SLEEP_TIME
+fi
+',
+}
+
+file { '/etc/consul/svc-traefik.json':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  source  => 'puppet:///nubis/files/svc-traefik.json',
+}
+
+# Normally, we should be able to have a static rule for this
+# But still tracking down a Traefik bug on that so we create
+# a synthethic service here for ourselves, grmbl
+
+file { '/etc/consul/svc-consul-ui.json':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///nubis/files/svc-consul-ui.json',
+}
+
