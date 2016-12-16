d19168952686db80ebe613adca03023a7ee6287d
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-12-16 11:33:08 -0500

    Only one health check per ELB is supported

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -452,14 +452,6 @@ resource "aws_elb" "traefik" {
     interval            = 30
   }
 
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "TCP:443"
-    interval            = 30
-  }
-
   cross_zone_load_balancing = true
 
   security_groups = [
