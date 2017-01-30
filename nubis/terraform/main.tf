ba4fbd20cd58b89f7dd228c51536de0dc9869800
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-30 10:13:02 -0500

    Switch root storate go gp2(SSD)
    Fixes #47

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -289,6 +289,8 @@ resource "aws_launch_configuration" "prometheus" {
 
   root_block_device = {
     volume_size = "32"
+    volume_type = "gp2"
+    delete_on_termination = true
   }
 
   security_groups = [
