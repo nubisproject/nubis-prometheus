6e99693d598270f8929bdc9c5fe31660f531168a
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-31 13:36:17 -0500

    Create a separete cloudwatch_exporter_billing for just AWS/Billing metrics
    Fixes #54

--- nubis/puppet/cloudwatch.pp
+++ nubis/puppet/cloudwatch.pp
@@ -1,5 +1,6 @@
 $cloudwatch_exporter_version = '0.3'
 $cloudwatch_exporter_port = 9116
+$cloudwatch_exporter_billing_port = 9117
 $cloudwatch_exporter_url = "https://github.com/gozer/cloudwatch_exporter/releases/download/GOZER-${cloudwatch_exporter_version}/cloudwatch_exporter-${cloudwatch_exporter_version}-GOZER-jar-with-dependencies.jar"
 
 notice ("Grabbing cloudwatch_exporter ${cloudwatch_exporter_version}")
@@ -33,4 +34,19 @@ upstart::job { 'cloudwatch_exporter':
     user           => 'root',
     group          => 'root',
     exec           => "java -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128 -jar /usr/share/cloudwatch_exporter/cloudwatch_exporter.jar ${cloudwatch_exporter_port} /etc/cloudwatch_exporter.yml",
+}
+
+upstart::job { 'cloudwatch_exporter_billing':
+    description    => 'CloudWatch Exporter for Billing',
+    service_ensure => 'stopped',
+    require        => [
+      Staging::File["cloudwatch.${cloudwatch_exporter_version}.jar"],
+    ],
+    # Never give up
+    respawn        => true,
+    respawn_limit  => 'unlimited',
+    start_on       => '(local-filesystems and net-device-up IFACE!=lo)',
+    user           => 'root',
+    group          => 'root',
+    exec           => "java -Dhttps.proxyHost=proxy.service.consul -Dhttps.proxyPort=3128 -jar /usr/share/cloudwatch_exporter/cloudwatch_exporter.jar ${cloudwatch_exporter_billing_port} /etc/cloudwatch_exporter_billing.yml",
 }
