da3c142d984b67ccf5cee0280fa7135d79211b7d
Nubisproject (Mozilla) <nubis-users@mozilla.com>
2019-03-06 23:16:32 +0000

    Update pinned Terraform version for v2.4.0 release [skip ci]

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "prometheus-image" {
-  source = "github.com/nubisproject/nubis-terraform//images?ref=v2.3.1"
+  source = "github.com/nubisproject/nubis-terraform//images?ref=v2.4.0"
 
   region        = "${var.aws_region}"
   image_version = "${var.nubis_version}"
